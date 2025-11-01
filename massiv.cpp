b026a92d8068155301aa7d60e0e39a24542fec21
k1r1ll <k1r1ll@k1r1ll.k1r1ll>
2025-11-01 13:34:32 +0300

    third commit

--- massiv.cpp
+++ massiv.cpp
@@ -23,14 +23,28 @@ int ** create (size_t a, size_t b){
     return mtx;
 }
 
+int ** construct(int ** mtx, int init, size_t a, size_t b){
+    for (size_t i = 0; i < a; ++i){
+        for (size_t j = 0; j < b; ++j){
+            mtx[i][j] = init;
+        }
+    }
+    return mtx;
+}
+
 int main(){
+    size_t a, b;
+    std::cin >> a >> b;
+    if (!std::cin){
+        return 2;
+    }
     int ** matrix = nullptr;
     try {
-        matrix = create(5, 5);
+        matrix = create(a, b);
     }
     catch(const std::bad_alloc & e){
         std::cerr << e.what();
         return 1;
     }
-    destroy(matrix, 8);
+    destroy(matrix, 5);
 }
